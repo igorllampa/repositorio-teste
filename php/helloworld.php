148d9323aed17e704272447c37780236350a1dde
igorllampa <igorllampa@gmail.com>
2020-05-24 20:49:50 -0300

    teste

--- php/helloworld.php
+++ php/helloworld.php
@@ -1,31 +1,8 @@
 <?php 
 echo 'Apenas um teste escrito na branch master';
-
-
-
-
 echo "Inicio";
 echo "Hello World!";
-
-echo "git-branch-modeulo-compras0";
-echo "git-branch-modeulo-compras0";
-echo "git-branch-modeulo-compras0";
-echo "git-branch-modeulo-compras0";
-echo "git-branch-modeulo-compras0";
-echo "git-branch-modeulo-compras0";
-echo "git-branch-modeulo-compras0";
-echo "git-branch-modeulo-compras0";
-
+echo "git-branch-modeulo-compras-teste";
 echo "Final do teste";
 echo "Esse é appenas um teste feito na branch modulo-financeiro. Indicacao de fim do trecho".;
-
-
-
-
-
-
-
-
-
-
 ?>
